a9c4d6cf8d7294b396e113a2a506f402cf5f6055
mosesjohn-hub <mosesjohn871862@gmail.com>
2025-12-07 10:23:12 -0800

    Update welcomepage.dart

---
+++ welcomepage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/flutter/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'welcome login-page',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+        ),
+        useMaterial3: true,
+      ),
+      home: const Login(),
+    );
+  }
+}
